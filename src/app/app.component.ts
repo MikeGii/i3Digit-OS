dfc97d19142be304dce2a40d96323cef2a3d352b
MikeGii <mikegii.prog@gmail.com>
2024-01-07 15:34:43 +0200

    Added button function (ALL) which helps to sort out filaments which are available in the stock and which are out of stock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FilamentService } from './service/filament.service';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { DataState } from './enum/data-state.enum';
@@ -9,12 +9,16 @@ import { Status } from './enum/status.enum';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
   readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private filamentService: FilamentService) { }
 
@@ -23,6 +27,7 @@ export class AppComponent implements OnInit {
     this.appState$ = this.filamentService.filaments$
       .pipe(
         map(response => {
+          this.dataSubject.next(response);
           return { dataState: DataState.LOADED_STATE, appData: response }
         }),
         startWith({ dataState: DataState.LOADING_STATE }),
@@ -31,4 +36,18 @@ export class AppComponent implements OnInit {
         })
       );
   }
+
+  filterFilaments(status: Status): void {
+    this.appState$ = this.filamentService.filter$(status, this.dataSubject.value)
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response}
+        }),
+        startWith({ dataState: DataState.LOADING_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
+  }
+
 }
